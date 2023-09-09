8acc1048e5eac5ecf78396370dd77692bed95f55
miggy-pg <meelan1323@gmail.com>
2023-09-09 08:56:18 +0800

    [UPDATE] add onclick types

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../assets/bootstrap/css/bootstrap-theme.css";
 import "../assets/bootstrap/css/bootstrap-theme.css.map";
 import "../assets/bootstrap/css/bootstrap-theme.min.css";
@@ -7,31 +7,50 @@ import "../assets/bootstrap/css/bootstrap.css";
 import "../assets/bootstrap/css/bootstrap.min.css";
 import "../assets/bootstrap/css/bootstrap.min.css.map";
 import "../assets/css/style.css";
+import "../assets/scss/style.scss";
 
 const apiKey = "EbXNorgMnC8zB/xZxM3CNg==FOyxrD2aAhQMHMXc";
-const apiUrl = "https://api.api-ninjas.com/v1/exercises";
 const headers = {
   "X-API-Key": apiKey,
 };
 
 function HomePage() {
+  const [selectedType, setSelectedType] = useState("");
+  const [exercises, setExercises] = useState([]);
+
   useEffect(() => {
     const makeAPICall = async () => {
       try {
-        const response = await fetch(apiUrl, {
-          mode: "cors",
-          headers,
-        });
-        await response.json().then((json) => {
-          console.log(json);
-        });
-      } catch (e) {
-        console.log(e);
-      }
+        const response = await fetch(
+          `https://api.api-ninjas.com/v1/exercises`,
+          {
+            mode: "cors",
+            headers,
+          }
+        );
+        await response
+          .json()
+          .then((json) =>
+            json.map((exercise, key) => ({ ...exercise, id: key }))
+          )
+          .then((res) => setExercises(res));
+      } catch (e) {}
     };
     makeAPICall();
   }, []);
 
+  // filter out exercise types
+  const typeList = [];
+  exercises.filter(
+    (arr, index, self) =>
+      index === self.findIndex((t) => t.type === arr.type) &&
+      typeList.push(arr.type)
+  );
+
+  const handleSelectType = (type) => {
+    setSelectedType(type);
+  };
+  console.log(selectedType);
   return (
     <section className="steps">
       <div className="container">
@@ -47,15 +66,29 @@ function HomePage() {
           </p>
           <div className="row">
             <div className="col-md-4">
-              <div className="text-center steps__single steps__single-first">
-                <img src="assets/images/step1.png" alt="" />
-                <p>Type</p>
+              <div className="text-center steps__single steps__single-first"></div>
+
+              <div className="exercise-types">
+                <p>Types</p>
+                <ol>
+                  {typeList.map((exercise, key) => (
+                    <li key={key} onClick={() => handleSelectType(exercise)}>
+                      <mark>{exercise.toUpperCase()}</mark>
+                    </li>
+                  ))}
+                </ol>
               </div>
             </div>
             <div className="col-md-4">
               <div className="text-center steps__single">
-                <img src="assets/images/step2.png" alt="" />
-                <p>LOREM IPSUM DOLOR SIT AMET</p>
+                <ol>
+                  {exercises.map(
+                    (exercise, key) =>
+                      exercise.type === selectedType && (
+                        <li key={key}>{exercise.name}</li>
+                      )
+                  )}
+                </ol>
               </div>
             </div>
             <div className="col-md-4">
